800e9e56b9140b6d88077c60842343f61d5a249f
Kerby <kn1578@nyu.edu>
2021-12-13 00:38:47 -0500

    More Juice TM
    But like budget

---
+++ Assets/heartComponent.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heartComponent : MonoBehaviour
+{
+    public ParticleSystem particles;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.A) == true) {
+            particles.Play();
+        }
+    }
+
+}
