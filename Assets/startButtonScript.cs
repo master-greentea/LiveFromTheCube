71af7263ecfddafbcb95888e7a1f2592dd4b3e30
Kerby <kn1578@nyu.edu>
2021-12-13 20:33:07 -0500

    camera ZOOM

---
+++ Assets/startButtonScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class startButtonScript : MonoBehaviour
+{
+
+    public Button startscreen;
+    public GameObject player;
+
+    Animator otherAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        startscreen.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void TaskOnClick() {
+        //Output this to console when Button1 or Button3 is clicked
+        Debug.Log("You have clicked the button!");
+
+        player.GetComponent<Animation>().Play();
+    }
+ }
