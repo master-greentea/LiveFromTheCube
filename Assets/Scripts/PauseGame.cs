b3e0070f3117aad0e55879afa497965fab823589
Wesley Davies <wtd224@nyu.edu>
2022-04-20 11:45:54 -0400

    Updated Client Matching UI

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    private bool _isPaused;
+    [SerializeField] private GameObject _pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!_isPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Unpause();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        _isPaused = true;
+        _pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    private void Unpause()
+    {
+        _isPaused = false;
+        _pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
